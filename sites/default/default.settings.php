57e8b82b1b58b6b9008e130e4780c5b427f9316e
Siegfried Neumann <quiptime@gmail.com>
2009-08-31 15:57:20 +0200

    Modules calendar and date added.

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -146,7 +146,7 @@ ini_set('session.use_cookies',      1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
-ini_set('memory_limit',             '92M');
+ini_set('memory_limit',             '96M');
 
 /**
  * Drupal automatically generates a unique session cookie name for each site
