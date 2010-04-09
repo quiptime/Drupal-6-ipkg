7e94fd1784d095afb64c15610a8f01c4739db0e5
Siegfried Neumann <quiptime@gmail.com>
2010-04-10 01:06:28 +0200

    Files reorganized.

--- sites/all/themes/zeropoint/box.tpl.php
+++
@@ -1,6 +0,0 @@
-<!-- box --> 
-<div class="box">
-  <?php if ($title) { ?><h2 class="title"><?php print $title ?></h2><?php } ?>
-  <div class="content"><?php print $content ?></div>
-</div>
-<!-- /box --> 
--- sites/all/themes/zeropoint/search-theme-form.tpl.php
+++
@@ -1,3 +0,0 @@
-<div id="search" class="container-inline">
-  <?php print $search_form; ?>
-</div>
